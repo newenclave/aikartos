2cc45e09a64d6e20cb266bfed0010e432890b12f
newenclave <newenclave@tutanota.com>
2025-05-17 20:49:42 +0300

    File format

--- aikartos/inc/aikartos/sch/scheduler_cfs_like.hpp
+++ aikartos/inc/aikartos/sch/scheduler_cfs_like.hpp
@@ -27,61 +27,61 @@
 
 namespace aikartos::sch {
 
-    namespace cfs_like {
+	namespace cfs_like {
 
-        enum class config_flags : std::uint32_t {
-        };
+		enum class config_flags : std::uint32_t {
+		};
 
-        template <typename ConfigT, typename TasksEventsType>
-        class scheduler {
-            struct vruntime_less;
-        public:
-            using config = ConfigT;
+		template<typename ConfigT, typename TasksEventsType>
+		class scheduler {
+			struct vruntime_less;
+		public:
+			using config = ConfigT;
 
-            constexpr static std::size_t maximum_tasks = config::maximum_tasks;
+			constexpr static std::size_t maximum_tasks = config::maximum_tasks;
 
-            using control_block = tasks::control_block;
-            using tasks_events_type = TasksEventsType;
+			using control_block = tasks::control_block;
+			using tasks_events_type = TasksEventsType;
 
-            struct scheduler_data_type {
-            	std::uint32_t vruntime = 0;
-            	std::uint32_t start = 0;
-            };
+			struct scheduler_data_type {
+				std::uint32_t vruntime = 0;
+				std::uint32_t start = 0;
+			};
 
-            using data_object_pool = utils::object_pool<scheduler_data_type, maximum_tasks, 4>;
-            using ready_tasks_queue = sync::stable_priority_queue<control_block *, maximum_tasks, vruntime_less, sync::policies::no_mutex>;
-            using waiting_queue = waiting_tasks_queue<maximum_tasks, sync::policies::no_mutex>;
+			using data_object_pool = utils::object_pool<scheduler_data_type, maximum_tasks, 4>;
+			using ready_tasks_queue = sync::stable_priority_queue<control_block *, maximum_tasks, vruntime_less, sync::policies::no_mutex>;
+			using waiting_queue = waiting_tasks_queue<maximum_tasks, sync::policies::no_mutex>;
 
-            void configure_task(control_block *task, const tasks::config &) {
-            	task->scheduler_data = static_cast<void *>(data_pool_.alloc());
-            }
+			void configure_task(control_block *task, const tasks::config&) {
+				task->scheduler_data = static_cast<void*>(data_pool_.alloc());
+			}
 
-            void clear_task(control_block *task) {
-            	data_pool_.free(get_data(task));
-            }
+			void clear_task(control_block *task) {
+				data_pool_.free(get_data(task));
+			}
 
-            control_block* get_next_task() {
-                process_waiting_queue();
-                const auto current_ticks = kernel::core::get_tick_count();
+			control_block* get_next_task() {
+				process_waiting_queue();
+				const auto current_ticks = kernel::core::get_tick_count();
 
-                // we have the current task on the top.
-                // recalculating task;
-                if(auto current = ready_tasks_.try_pop()) {
-                	auto *task = *current;
-                	auto data = get_data(task);
-                	if(data->start != 0) {
-                    	data->vruntime += (current_ticks - data->start);
-                	}
-                	ready_tasks_.try_push(task);
-                }
+				// we have the current task on the top.
+				// recalculating task;
+				if (auto current = ready_tasks_.try_pop()) {
+					auto *task = *current;
+					auto data = get_data(task);
+					if (data->start != 0) {
+						data->vruntime += (current_ticks - data->start);
+					}
+					ready_tasks_.try_push(task);
+				}
 #ifdef DEBUG
-                fill_vrun_times();
+				fill_vrun_times();
 #endif
-                // try the next task
-				while(auto next_task = ready_tasks_.peek()) {
+				// try the next task
+				while (auto next_task = ready_tasks_.peek()) {
 					auto *task = *next_task;
 
-					switch(task->task.state) {
+					switch (task->task.state) {
 					case tasks::descriptor::state_type::READY:
 						[[fallthrough]];
 					case tasks::descriptor::state_type::RUNNING:
@@ -104,48 +104,50 @@ namespace aikartos::sch {
 					}
 				}
 
-                return nullptr;
-            }
+				return nullptr;
+			}
 
-            void add_task(control_block *task) {
-            	ready_tasks_.try_push(task);
+			void add_task(control_block *task) {
+				ready_tasks_.try_push(task);
 #ifdef DEBUG
-                fill_vrun_times();
+				fill_vrun_times();
 #endif
-            }
+			}
 
-        private:
+		private:
 
-            struct vruntime_less {
-            	bool operator ()(control_block *lhs, control_block *rhs) const {
-            		return get_data(rhs)->vruntime < get_data(lhs)->vruntime;
-            	}
-            };
+			struct vruntime_less {
+				bool operator ()(control_block *lhs, control_block *rhs) const {
+					return get_data(rhs)->vruntime < get_data(lhs)->vruntime;
+				}
+			};
 
-            static scheduler_data_type *get_data(control_block *task) {
-            	return task->get_scheduler_data<scheduler_data_type>();
-            }
+			static scheduler_data_type* get_data(control_block *task) {
+				return task->get_scheduler_data<scheduler_data_type>();
+			}
 
-            void process_waiting_queue() {
-                waiting_tasks_.process([this](auto *task) { add_task(task); });
-            }
+			void process_waiting_queue() {
+				waiting_tasks_.process([this](auto *task) {
+					add_task(task);
+				});
+			}
 
 #ifdef DEBUG
-            void fill_vrun_times() {
-            	std::size_t id = 0;
-            	ready_tasks_.foreach([this, &id](control_block *task) {
-            		current_vruns[id++] = get_data(task)->vruntime;
-            	});
-            	for(; id<maximum_tasks; ++id) {
-            		current_vruns[id] = 0;
-            	}
-            }
-            std::uint32_t current_vruns[maximum_tasks];
+			void fill_vrun_times() {
+				std::size_t id = 0;
+				ready_tasks_.foreach([this, &id](control_block *task) {
+					current_vruns[id++] = get_data(task)->vruntime;
+				});
+				for (; id < maximum_tasks; ++id) {
+					current_vruns[id] = 0;
+				}
+			}
+			std::uint32_t current_vruns[maximum_tasks];
 #endif
-            ready_tasks_queue ready_tasks_;
-            waiting_queue waiting_tasks_;
-            data_object_pool data_pool_;
-        };
+			ready_tasks_queue ready_tasks_;
+			waiting_queue waiting_tasks_;
+			data_object_pool data_pool_;
+		};
 
-    }
+	}
 }
